0166bad0cb31df7797daabea7793e19a4952a412
30carterjack <jc4078@bath.ac.uk>
2024-02-27 13:24:26 +0000

    handle child proccess

--- container.c
+++ container.c
@@ -31,7 +31,7 @@ int choose_hostname(char *buff, size_t len) {
     static const char *suits[] = {"swords", "cups", "coins", "wands"};
     static const char *minor[] = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                                   "page", "knight", "queen", "king"};
-    static const char *major[] = {"the fool", "the magician", "the high priestess", "the empress", "the emperor",
+    static const char *major[] = {"the fool", "the magician", "the high priestess", "the empress", "the emperor"};
     struct timespec now = {0};
     clock_gettime(CLOCK_MONOTONIC, &now);
     size_t ix = now.tv_nsec % 78;
@@ -43,13 +43,69 @@ int choose_hostname(char *buff, size_t len) {
     }
     return 0;
 }
+
+void cleanup_resources(int sockets[2], char *stack) {
+    if (sockets[0]) close(sockets[0]);
+    if (sockets[1]) close(sockets[1]);
+    free(stack);
+}
+
+void error_cleanup(int sockets[2], char *stack) {
+    cleanup_resources(sockets, stack);
+    // Additional error handling and cleanup if needed
+}
+
+#define USERNS_OFFSET 100000
+#define USERNS_COUNT 200000
+
+int handle_child_process_uid_map(pid_t child_process_pid, int fd) {
+    int uid_map = 0;
+    int has_userns = -1;
+    if (read(fd, &has_userns, sizeof(has_userns)) != sizeof(has_userns)) {
+        fprintf(stderr, "Failed to read from child process: %s\n", strerror(errno));
+        return -1;
+    }
+    if (has_userns) {
+        char path[PATH_MAX] = {0};
+        for (char **file = (char *[]) {"uid_map", "gid_map", 0}; *file; file++) {
+            if (snprintf(path, sizeof(path), "/proc/%d/%s", child_process_pid, *file) < 0) {
+                fprintf(stderr, "Failed to create path: %s\n", strerror(errno));
+                return -1;
+            }
+            fprintf(stderr, "Writing to %s...\n", path);
+            if ((uid_map = open(path, O_WRONLY)) < 0) {
+                fprintf(stderr, "Failed to open: %s\n", path, strerror(errno));
+                return -1;
+            }
+            if (dprintf(uid_map, "0 %d %d\n", USERNS_OFFSET, USERNS_COUNT) == -1) {
+                fprintf(stderr, "Failed to write to %s: %s\n", path, strerror(errno));
+                close(uid_map);
+                return -1;
+            }
+            close(uid_map);
+        }
+    }
+    if (write(fd, & (int) {0}, sizeof(int)) != sizeof(int)) {
+        fprintf(stderr, "Failed to write to child process: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int userns(struct child_process_config *config) {
+    fprintf(stderr, "Entering user namespace...\n");
+    int has_userns !unshare(CLONE_NEWUSER);
+}
+
+
+
 int main(int argc, char **argv) {
     struct child_process_config config = {0};
     int err = 0;
     int option = 0;
     int sockets[2] = {0};
-    pid_t child_proccess_pid = 0;
-    int last_optind = 0;
+    pid_t child_process_pid = 0;
+    char *stack = NULL;
 
     while ((option = getopt(argc, argv, "c:m:u:")) != -1) {
         switch (option) {
@@ -57,7 +113,6 @@ int main(int argc, char **argv) {
                 config.argc = argc - optind;
                 config.argv = &argv[optind];
                 goto finish_options;
-                break;
             case 'm':
                 config.mount_dir = optarg;
                 break;
@@ -70,11 +125,12 @@ int main(int argc, char **argv) {
             default:
                 goto usage;
         }
-        last_optind = optind;
     }
 finish_options:
-    if (!config.argc) goto usage;
-    if (!config.mount_dir) goto usage;
+    if (!config.argc || !config.mount_dir) {
+        fprintf(stderr, "Missing arguments.\n");
+        goto usage;
+    }
 
     /* Validate macOS version */
     struct utsname host = {0};
@@ -96,20 +152,21 @@ finish_options:
 
     char hostname[256] = {0};
     if (choose_hostname(hostname, sizeof(hostname)) < 0) {
-        goto error;
+        fprintf(stderr, "Failed to generate hostname.\n");
+        goto cleanup;
     }
     config.hostname = hostname;
 
     /* Perform other operations (namespaces, etc.) */
 
+    // Place your additional code here, such as creating namespaces, setting up resources, etc.
+
 cleanup:
-    if (sockets[0]) close(sockets[0]);
-    if (sockets[1]) close(sockets[1]);
+    cleanup_resources(sockets, stack);
     return err;
 
 usage:
     fprintf(stderr, "Usage: %s -u -1 -m . -c /bin/sh ~\n", argv[0]);
-error:
     err = 1;
     goto cleanup;
 }
