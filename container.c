84a574184dde11a78c6fc18e5e28c10c5e9cad71
30carterjack <jc4078@bath.ac.uk>
2024-02-27 11:47:09 +0000

    Choose hostname func

--- container.c
+++ container.c
@@ -25,21 +25,91 @@ struct child_process_config {
     char *mount_dir;
 };
 
-<<capabilities>>
+/* Define the capabilities, mounts, syscalls, resources, and child_process functions */
 
-<<mounts>>
-
-<<syscalls>>
-
-<<resources>>
-
-<<child>>
-
-<<choose-hostname>>
-
+int choose_hostname(char *buff, size_t len) {
+    static const char *suits[] = {"swords", "cups", "coins", "wands"};
+    static const char *minor[] = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                                  "page", "knight", "queen", "king"};
+    static const char *major[] = {"the fool", "the magician", "the high priestess", "the empress", "the emperor",
+    struct timespec now = {0};
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    size_t ix = now.tv_nsec % 78;
+    if (ix < sizeof(major) / sizeof(*major)) {
+        snprintf(buff, len, "%05lx-%s", now.tv_sec, major[ix]);
+    } else {
+        ix -= sizeof(major) / sizeof(*major);
+        snprintf(buff, len, "%05lxc-%s-of-%s", now.tv_sec, minor[ix % (sizeof(minor) / sizeof(*minor))], suits[ix / (sizeof(minor) / sizeof(*minor))]);
+    }
+    return 0;
+}
 int main(int argc, char **argv) {
     struct child_process_config config = {0};
+    int err = 0;
+    int option = 0;
+    int sockets[2] = {0};
+    pid_t child_proccess_pid = 0;
+    int last_optind = 0;
 
-    printf("Hello, world!\n");
-    return 0;
+    while ((option = getopt(argc, argv, "c:m:u:")) != -1) {
+        switch (option) {
+            case 'c':
+                config.argc = argc - optind;
+                config.argv = &argv[optind];
+                goto finish_options;
+                break;
+            case 'm':
+                config.mount_dir = optarg;
+                break;
+            case 'u':
+                if (sscanf(optarg, "%d", &config.uid) != 1) {
+                    fprintf(stderr, "Invalid uid: %s\n", optarg);
+                    goto usage;
+                }
+                break;
+            default:
+                goto usage;
+        }
+        last_optind = optind;
+    }
+finish_options:
+    if (!config.argc) goto usage;
+    if (!config.mount_dir) goto usage;
+
+    /* Validate macOS version */
+    struct utsname host = {0};
+    if (uname(&host) < 0) {
+        fprintf(stderr, "Failed to get host information: %s\n", strerror(errno));
+        goto cleanup;
+    }
+    int major = -1;
+    int minor = -1;
+    if (sscanf(host.release, "%d.%d", &major, &minor) != 2) {
+        fprintf(stderr, "Failed to parse host release: %s\n", host.release);
+        goto cleanup;
+    }
+    if (major < 10) {
+        fprintf(stderr, "Unsupported macOS version: %s\n", host.release);
+        goto cleanup;
+    }
+    fprintf(stderr, "%s on %s\n", host.release, host.sysname);
+
+    char hostname[256] = {0};
+    if (choose_hostname(hostname, sizeof(hostname)) < 0) {
+        goto error;
+    }
+    config.hostname = hostname;
+
+    /* Perform other operations (namespaces, etc.) */
+
+cleanup:
+    if (sockets[0]) close(sockets[0]);
+    if (sockets[1]) close(sockets[1]);
+    return err;
+
+usage:
+    fprintf(stderr, "Usage: %s -u -1 -m . -c /bin/sh ~\n", argv[0]);
+error:
+    err = 1;
+    goto cleanup;
 }
